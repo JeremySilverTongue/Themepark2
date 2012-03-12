bea0e8cdd431d1b7c119f29a8cadce0ff8f9779c
Jeremy Silver <jeremy_silver@apple.com>
2012-03-12 06:28:32 -0700

    Initial Commit

---
+++ Themepark2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Themepark2
+//
+//  Created by Jeremy Silver on 3/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
